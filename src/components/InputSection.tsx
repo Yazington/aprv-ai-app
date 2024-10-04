a37c9a148094f23bac7694cc55f053f9470d0ea4
Yazington <binaryaz@gmail.com>
2024-10-03 23:17:33 -0400

    added small bugfix

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,6 +1,6 @@
 interface Props {
   input: string | null;
-  setInput: (value: string) => void;
+  setInput: React.Dispatch<React.SetStateAction<string | null>>;
   handleSend: () => void;
 }
 
@@ -8,14 +8,16 @@ const InputSection = ({ input, setInput, handleSend }: Props) => {
   return (
     <div className="p-4 border-t border-gray-700">
       <div className="flex">
-        <input
-          type="text"
+        <textarea
           className="flex-1 bg-gray-800 border border-gray-700 rounded px-4 py-2 text-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-500"
           placeholder="Type your message..."
           value={input ? input : ''}
-          onChange={e => setInput(e.target.value)}
+          onChange={e => {
+            setInput(e.target.value);
+            console.log('Input changed:', e.target.value); // Debugging
+          }}
           onKeyDown={e => {
-            if (e.key === 'Enter') {
+            if (e.key === 'Enter' && !e.shiftKey) {
               e.preventDefault();
               handleSend();
             }
