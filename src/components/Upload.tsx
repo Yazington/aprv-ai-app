1416b9914f529214d2b7e8dea782ec1639d5781e
Yazington <binaryaz@gmail.com>
2024-10-24 16:05:25 -0400

    added loading

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -27,13 +27,16 @@ export default () => {
       selectedConversation: state.selectedConversation,
     }))
   );
+  const [isLoading, setIsLoading] = useState<boolean | undefined>(undefined);
 
   const [designFiles, setDesignFiles] = useState<File[]>([]);
   const [otherFiles, setOtherFiles] = useState<File[]>([]);
   // TODO: check if we need to save those files on disk instead
   const isDesignAlreadyUploaded = selectedConversation?.design_id ? true : false;
   const isGuidelineAlreadyUploaded = selectedConversation?.contract_id ? true : false;
-
+  // const isLoading = designFiles?.length > 0 && isGuidelineAlreadyUploaded == false;
+  console.log('up' + isGuidelineAlreadyUploaded);
+  console.log(designFiles?.length);
   useEffect(() => {
     if (userId) {
       apiClient.get(`/conversations?user_id=${userId}`).then(response => setAllUserConversations(response.data));
@@ -52,17 +55,14 @@ export default () => {
 
   const handleFileUpload = async (fileList: FileList, isDesign: boolean) => {
     if (!fileList || fileList.length === 0) return;
-
+    setIsLoading(true);
     try {
       for (let i = 0; i < fileList.length; i++) {
         const file = fileList[i];
         if (!file) continue;
         const formData = new FormData();
         formData.append('file', file);
-        // if (!!currentlySelectedConversationId) {
-        //   console.log('Selected conversation id is undefined');
-        //   return;
-        // }
+
         let uploadUrl = `/upload/image?conversation_id=${currentlySelectedConversationId}`;
         if (file.type === 'application/pdf') {
           uploadUrl = `/upload/pdf?conversation_id=${currentlySelectedConversationId}`;
@@ -81,14 +81,16 @@ export default () => {
         if (response.data) {
           setCurrentConversationId(response.data.conversation_id);
         }
-
+        setIsLoading(false);
         console.log(`File ${i + 1} uploaded successfully!`);
       }
     } catch (error) {
       console.error(error);
+      setIsLoading(undefined);
       alert('Failed to upload one or more files.');
     }
   };
+  console.log(isLoading);
   // Giving percentage size for this will add sliding window -> we can fix it by making sure the sizes of element within the container arent overflowing
   return (
     <div className="flex min-w-0 basis-[10%]">
@@ -153,10 +155,34 @@ export default () => {
             </div>
           )}
         </div>
-        <div className="flex min-w-0 basis-2/12 flex-col truncate">
-          <div className="text-pretty text-center">Guideline Upload</div>
+        <div className="flex w-full min-w-0 basis-2/12 flex-col items-center justify-center truncate">
+          <div className="w-full items-center justify-center text-pretty text-center">Guideline Upload</div>
           {isGuidelineAlreadyUploaded && selectedConversation?.contract_id}
-          {!isGuidelineAlreadyUploaded && (
+          {isLoading && (
+            <div
+              role="status"
+              className="w-full items-center justify-center"
+            >
+              <svg
+                aria-hidden="true"
+                className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          )}
+          {!isLoading && !isGuidelineAlreadyUploaded && (
             <div>
               <div className="overflow-y-auto">
                 {otherFiles.length > 0 && (
