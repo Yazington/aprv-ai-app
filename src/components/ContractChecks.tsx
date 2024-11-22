cb58bfb507f972cc5be531c2ce68eeb1822a9f12
Yazington <binaryaz@gmail.com>
2024-11-22 17:39:03 -0500

    changing button name

--- src/components/ContractChecks.tsx
+++ src/components/ContractChecks.tsx
@@ -114,61 +114,63 @@ export default () => {
           <h1 className="text-xl font-semibold tracking-tight text-textPrimary">Guidelines Checks</h1>
         </div>
         <div className="w-full basis-[88%] items-center justify-center divide-y overflow-y-auto">
-          {reviews.map(review => (
-            <div
-              className="flex min-h-[24px] w-full flex-row items-center justify-evenly p-2 text-xl"
-              key={review.page_number}
-            >
-              <span className="font-mono">Page {review.page_number}</span>
-              <Tippy
-                content={<ReactMarkdown>{review.review_description}</ReactMarkdown>}
-                arrow={true}
-                className="bg-darkBg3"
-                placement="right"
+          {reviews
+            .sort((a, b) => a.page_number - b.page_number) // Sort by page_number
+            .map(review => (
+              <div
+                className="flex min-h-[24px] w-full flex-row items-center justify-evenly p-2 text-xl"
+                key={review.page_number}
               >
-                <div
-                  className={`${IconStyles.base} ${
-                    review.guideline_achieved == null && review.guideline_achieved !== false
-                      ? IconStyles.alert
-                      : review.guideline_achieved
-                        ? IconStyles.success
-                        : IconStyles.error
-                  }`}
+                <span className="font-mono">Page {review.page_number}</span>
+                <Tippy
+                  content={<ReactMarkdown>{review.review_description}</ReactMarkdown>}
+                  arrow={true}
+                  className="bg-darkBg3"
+                  placement="right"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="16" // Adjust the width
-                    height="16" // Adjust the height
-                    fill="currentColor"
-                    className={IconStyles.svgStyles}
+                  <div
+                    className={`${IconStyles.base} ${
+                      review.guideline_achieved == null && review.guideline_achieved !== false
+                        ? IconStyles.alert
+                        : review.guideline_achieved
+                          ? IconStyles.success
+                          : IconStyles.error
+                    }`}
                   >
-                    {review.guideline_achieved == null && review.guideline_achieved !== false && (
-                      <path
-                        fillRule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-                        clipRule="evenodd"
-                      />
-                    )}
-                    {review.guideline_achieved && (
-                      <path
-                        fillRule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                        clipRule="evenodd"
-                      />
-                    )}
-                    {review.guideline_achieved !== null && review.guideline_achieved === false && (
-                      <path
-                        fillRule="evenodd"
-                        d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                        clipRule="evenodd"
-                      />
-                    )}
-                  </svg>
-                </div>
-              </Tippy>
-            </div>
-          ))}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className={IconStyles.svgStyles}
+                    >
+                      {review.guideline_achieved == null && review.guideline_achieved !== false && (
+                        <path
+                          fillRule="evenodd"
+                          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                          clipRule="evenodd"
+                        />
+                      )}
+                      {review.guideline_achieved && (
+                        <path
+                          fillRule="evenodd"
+                          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                          clipRule="evenodd"
+                        />
+                      )}
+                      {review.guideline_achieved !== null && review.guideline_achieved === false && (
+                        <path
+                          fillRule="evenodd"
+                          d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                          clipRule="evenodd"
+                        />
+                      )}
+                    </svg>
+                  </div>
+                </Tippy>
+              </div>
+            ))}
         </div>
         <div className="flex flex-[10%] items-center justify-center">
           {!processingHasStarted && (
@@ -176,7 +178,7 @@ export default () => {
               className="flex max-h-10 items-center justify-center rounded-full bg-buttonBlack px-4 py-2 font-bold text-textSecondary transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-slate-900"
               onClick={processDesign}
             >
-              PDAC
+              Full Compliance Check
             </button>
           )}
           {processingHasStarted && isProcessing && (
